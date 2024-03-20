66c976f4e79d56ac640757e5698af16a8d4b83c9
Vi-c-hu <120244320+Vi-c-hu@users.noreply.github.com>
2024-03-20 09:27:21 +0530

    Add files via upload

---
+++ calculateSquareAndCube.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class calculateSquareAndCube
+{
+	public static void main(string[]args)
+	{
+		double number = 2.5;
+		double number = 5.6;
+		double square = math.pow(number,2);
+		double cube = math.pow(number,3);
+		System.out.println("sqaree of" +number+" is:"+square);
+		System.out.println("cube of"+number+"is",cube);
+	}
+}
